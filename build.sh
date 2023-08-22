bc6fcd5fd86cbe72cd7b7f7a637af066163ea4e7
Stephan Vedder <vedder@mbits.info>
2023-08-22 16:10:14 +0200

    Autogenerate some pat files

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VERSION=1.3
+FOLDER_NAME=zlib-$VERSION
+ARCHIVE_NAME=zlib-$VERSION.tar.gz
+LIB_NAME=libz.a
+MIRROR=https://www.zlib.net/fossils/
+
+declare -a TOOLCHAINS=("linux_gcc_i686" "linux_clang_i686" "linux_gcc_x64" "linux_clang_x64")
+
+wget -nc $MIRROR$ARCHIVE_NAME
+tar -xf $ARCHIVE_NAME
+
+for TOOLCHAIN in "${TOOLCHAINS[@]}"
+do
+  mkdir -p build-$TOOLCHAIN
+  cmake -S $FOLDER_NAME -B build-$TOOLCHAIN -DCMAKE_INSTALL_PREFIX=install-$TOOLCHAIN -DCMAKE_TOOLCHAIN_FILE=../toolchains/$TOOLCHAIN.cmake -G Ninja
+  ninja -C build-$TOOLCHAIN install
+  mkdir -p objects-$TOOLCHAIN
+  ar x ./install-$TOOLCHAIN/lib/$LIB_NAME --output objects-$TOOLCHAIN
+  find objects-$TOOLCHAIN/ -type f -name "*.o" | xargs -I % rz-sign -o %.pat %
+  python generate-pat.py --auto --recursive -d objects-$TOOLCHAIN/ -o zlib-$TOOLCHAIN.pat
+done
---
+++ toolchains/linux_clang_i686.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_FLAGS --target=i686-linux-elf)
